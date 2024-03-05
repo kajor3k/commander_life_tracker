e9e72ac56fffb93bc68714312fb3cf5c9763c590
Kaj <kajor3k@gmail.com>
2024-03-05 16:38:31 +0100

    #006 minor refactor, initial commit for left pane

--- commander_life_tracker.cpp
+++ commander_life_tracker.cpp
@@ -3,6 +3,7 @@
 #include "libraries/pico_graphics/pico_graphics.hpp"
 #include "pico/stdlib.h"
 #include "libraries/gfx_pack/gfx_pack.hpp"
+#include <list>
 
 using namespace pimoroni;
 
@@ -14,12 +15,70 @@ Button button_b(GfxPack::B);
 Button button_c(GfxPack::C);
 Button button_d(GfxPack::D);
 Button button_e(GfxPack::E);
-Point main_label_location(58, 25);
-Point main_location(64, 32);
+Point left_pane_start_location (0,0);
+Point right_pane_start_location (98, 0);
+Point main_label_location(40, 0);
+//Point main_label_location(48, 20);
+Point main_location(54, 33);
 
-std::string convert_life_to_string(int life) {
-     std::string lifeString = std::to_string(life);
-     return lifeString;
+std::string commander_string = "comm_dmg";
+std::string life_label = "Life";
+std::string commander_damage_0_label = commander_string +" 0";
+std::string commander_damage_1_label = commander_string +" 1";
+std::string commander_damage_2_label = commander_string +" 2";
+std::string commander_damage_3_label = commander_string +" 3";
+std::string commander_damage_4_label = commander_string +" 4";
+std::string commander_damage_5_label = commander_string +" 5";
+std::string commander_damage_6_label = commander_string +" 6";
+std::string commander_damage_7_label = commander_string +" 7";
+std::string commander_damage_8_label = commander_string +" 8";
+std::string commander_tax_label = "comm_tax";
+std::string generic_counter_label = "Generic";
+std::string energy_counter_label = "Energy";
+std::string poison_counter_label = "Poison";
+std::string experience_counter_label = "Exp";
+std::string the_ring_counter_label = "The Ring";
+std::string storm_counter_label = "Storm";
+
+std::vector<std::tuple<std::string, int>> get_counter_labels_vector() {
+    std::vector<std::tuple<std::string, int>> counter_labels_vector;
+    std::unordered_map<std::string, int> counters_labels_map;
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(life_label, 40));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_0_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_1_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_2_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_3_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_4_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_5_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_6_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_7_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_damage_8_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(commander_tax_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(generic_counter_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(energy_counter_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(poison_counter_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(experience_counter_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(the_ring_counter_label, 0));
+    counter_labels_vector.emplace_back(std::pair<std::string, int>(storm_counter_label, 0));
+    return counter_labels_vector;
+}
+
+std::vector<std::tuple<std::string, int>> counter_labels_vector = get_counter_labels_vector();
+
+
+
+
+
+
+
+int current_counter = 40;
+std::string current_label = life_label;
+
+void load_screen();
+
+std::string int_to_string(int current) {
+     std::string final_string = std::to_string(current);
+     return final_string;
 }
 
 void clean_screen() {
@@ -27,83 +86,143 @@ void clean_screen() {
     graphics.clear();
 }
 
-enum active_view
-{
-    life_view = 0,
-    commander_tax_view = 1,
-    generic_counter_view = 2
-    //TODO add all views
+bool check_if_commander_damage(std::string key) {
+    std::string substring = "commander_damage";
+    size_t match = key.find(substring);
+    return match;
+}
 
-};
+bool check_if_commander_tax(std::string key) {
+    std::string substring = "commander_tax";
+    size_t match = key.find(substring);
+    return match;
+}
+
+void load_main_screen(Point label_location) {
+    //graphics.set_dimensions(5,5);
+    //graphics.update_pen()
+    graphics.set_pen(15);
+    graphics.text(current_label, label_location, 21);
+    graphics.text(int_to_string(current_counter), main_location, 320);
+    st7567.update(&graphics);
+}
 
 
+
+
+void load_left_pane(){
+    // upper left corner x and y, lower right corner x and y (not straightforward, w stands for wrapping, h for height, but effectively it's an end point coordinates)
+    Rect pane_rect(0,0,29,64);
+    graphics.rectangle(pane_rect);
+    auto inner_counter_it = counter_labels_vector.begin();
+
+
+    //pane_rect.deflate(5);
+    graphics.set_pen(15);
+    graphics.text(std::get<0>(counter_labels_vector[1]), Point(pane_rect.x, pane_rect.y), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[1]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[2]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[2]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[3]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[3]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[4]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[4]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[5]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[5]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[6]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[6]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[7]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(std::get<0>(counter_labels_vector[7]), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+    graphics.text(int_to_string(std::get<1>(counter_labels_vector[1])), Point(pane_rect.x, pane_rect.y+5), pane_rect.w);
+
+    st7567.update(&graphics);
+}
 int main() {
 
-    int life_count = 40;
-    int commander_damage_0 = 0;
-    int commander_damage_1 = 0;
-    int commander_damage_2 = 0;
-    int commander_damage_3 = 0;
-    int commander_damage_4 = 0;
-    int commander_damage_6 = 0;
-    int commander_damage_7 = 0;
-    int commander_damage_8 = 0;
-    int commander_tax = 0;
-    int generic_counter = 0;
-    int energy_counter = 0;
-    int poison_counter = 0;
-    int experience_counter = 0;
-    int the_ring_counter = 0;
-    int storm_counter = 0;
 
-    int current_view = active_view(life_view);
 
+
+    auto counter_it = counter_labels_vector.begin();
+
+    st7567.set_backlight(64);
 
 
     while(true) {
 
         //previous tab
         if(button_a.read()){
-            for (int i = current_view; i >= active_view(life_view); i-- ){
-                if (current_view == active_view(life_view)){
-                    current_view = active_view(generic_counter_view);
-                }
+
+            if (counter_it != counter_labels_vector.begin()){
+                --counter_it;
+                current_counter = std::get<1>(*counter_it);
             }
+            current_label = std::get<0>(*counter_it);
+
+
         }
 
         //next tab
         if(button_b.read()){
-            for (int i = current_view; i <= active_view(generic_counter_view); i++ ){
-                if (current_view == active_view(generic_counter_view)){
-                    current_view = active_view(life_view);
-                }
+            if (counter_it != counter_labels_vector.end()-1){
+                ++counter_it;
+                current_counter = std::get<1>(*counter_it);
             }
+            current_label = std::get<0>(*counter_it);
+
+
+
         }
 
         //check for only one press
         //button.raw will update the state smoothly, which is not desired here
         //button.read check for state before update
 
-        //if(button_d.read()) {
-          //  life_count -= 1;
-       // }
+        if(button_d.read()) {
+            current_counter -= 1;
+            std::get<1>(*counter_it) = current_counter;
 
-        //if(button_e.read()) {
-            //life_count += 1;
-      //  }
+        }
 
-        //clean screen
-        //TODO: move those lines to distinct function as it will be called multiple times
+        if(button_e.read()) {
+            if(current_label.find("comm_tax") != std::string::npos){
+                current_counter += 1;
+            }
+            current_counter += 1;
+            std::get<1>(*counter_it) = current_counter;
+            if(current_label.find("comm_dmg") != std::string::npos){
+                //if commander damage, then subtract life
+
+                std::get<1>(counter_labels_vector.front()) -= 1;
+            }
+
+        }
+
+
         clean_screen();
 
         //update screen
         //TODO: wrap up in method
-        graphics.set_pen(15);
         // load screen 1
-        graphics.text("Life", main_label_location, 320);
-        graphics.text(convert_life_to_string(life_count), main_location, 320);
+        load_main_screen(main_label_location);
+        //load_left_pane();
 
-        st7567.update(&graphics);
+        //TODO drop instruction
 
     }
 }
+
+
+
